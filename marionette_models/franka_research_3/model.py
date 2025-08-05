8172126a4041aa5ac515db2335f5bd0a51d7559c
David Dorf <daviddorf2023@u.northwestern.edu>
2025-08-05 18:17:56 -0400

    refactor, working examples

--- marionette_models/franka_research_3/model.py
+++ marionette_models/franka_research_3/model.py
@@ -1,23 +1,24 @@
-from marionette.format import Link, RigidJoint, RevoluteJoint, SliderJoint, Transform, Model
+from marionette.format import Link, RigidJoint, RevoluteJoint, SliderJoint, Pose, Model
 from math import pi
+import os
 
 
-link0 = Link(name = 'link0', visual = "visual/link0.glb")
-link1 = Link(name = 'link1', visual = "visual/link1.glb")
-link2 = Link(name = 'link2', visual = "visual/link2.glb")
-link3 = Link(name = 'link3', visual = "visual/link3.glb")
-link4 = Link(name = 'link4', visual = "visual/link4.glb")
-link5 = Link(name = 'link5', visual = "visual/link5.glb")
-link6 = Link(name = 'link6', visual = "visual/link6.glb")
-link7 = Link(name = 'link7', visual = "visual/link7.glb")
-hand = Link(name = 'hand', visual = "visual/hand.glb")
-finger1 = Link(name = 'finger1', visual = "visual/finger.glb")
-finger2 = Link(name = 'finger2', visual = "visual/finger.glb")
+link0 = Link(name = 'link0', visual = "assets/link0.glb")
+link1 = Link(name = 'link1', visual = "assets/link1.glb")
+link2 = Link(name = 'link2', visual = "assets/link2.glb")
+link3 = Link(name = 'link3', visual = "assets/link3.glb")
+link4 = Link(name = 'link4', visual = "assets/link4.glb")
+link5 = Link(name = 'link5', visual = "assets/link5.glb")
+link6 = Link(name = 'link6', visual = "assets/link6.glb")
+link7 = Link(name = 'link7', visual = "assets/link7.glb")
+hand = Link(name = 'hand', visual = "assets/hand.glb")
+finger1 = Link(name = 'finger1', visual = "assets/finger.glb")
+finger2 = Link(name = 'finger2', visual = "assets/finger.glb")
 
 joint0 = RevoluteJoint(
     parent = link0,
     child = link1,
-    transform = Transform(z = 0.333),
+    pose = Pose(translation = (0, 0, 0.333)),
     axis = (0, 0, 1),
     limits = (-2.8973, 2.8973),
 )
@@ -25,7 +26,7 @@ joint0 = RevoluteJoint(
 joint1 = RevoluteJoint(
     parent = link1,
     child = link2,
-    transform = Transform(),
+    pose = Pose(),
     axis = (0, 1, 0),
     limits = (-1.7628, 1.7628),
 )
@@ -33,7 +34,7 @@ joint1 = RevoluteJoint(
 joint2 = RevoluteJoint(
     parent = link2,
     child = link3,
-    transform = Transform(z = 0.316),
+    pose = Pose(translation = (0, 0, 0.316)),
     axis = (0, 0, 1),
     limits = (-2.8973, 2.8973),
 )
@@ -41,7 +42,7 @@ joint2 = RevoluteJoint(
 joint3 = RevoluteJoint(
     parent = link3,
     child = link4,
-    transform = Transform(x = 0.0825),
+    pose = Pose(translation = (0.0825, 0, 0)),
     axis = (0, -1, 0),
     limits = (-3.0718, -0.0696),
 )
@@ -49,7 +50,7 @@ joint3 = RevoluteJoint(
 joint4 = RevoluteJoint(
     parent = link4,
     child = link5,
-    transform = Transform(x = -0.0825, z = 0.384),
+    pose = Pose(translation = (-0.0825, 0, 0.384)),
     axis = (0, 0, 1),
     limits = (-2.8973, 2.8973),
 )
@@ -57,7 +58,7 @@ joint4 = RevoluteJoint(
 joint5 = RevoluteJoint(
     parent = link5,
     child = link6,
-    transform = Transform(),
+    pose = Pose(),
     axis = (0, -1, 0),
     limits = (-0.0175, 3.7525),
 )
@@ -65,7 +66,7 @@ joint5 = RevoluteJoint(
 joint6 = RevoluteJoint(
     parent = link6,
     child = link7,
-    transform = Transform(x = 0.088),
+    pose = Pose(translation = (0.088, 0, 0)),
     axis = (0, 0, 1),
     limits = (-2.8973, 2.8973)
 )
@@ -73,13 +74,13 @@ joint6 = RevoluteJoint(
 joint7 = RigidJoint(
     parent = link7,
     child = hand,
-    transform = Transform(z = -0.107)
+    pose = Pose(translation = (0, 0, -0.107))
 )
 
 joint8 = SliderJoint(
     parent = hand,
     child = finger1,
-    transform = Transform(z = -0.0584),
+    pose = Pose(translation = (0, 0, -0.0584)),
     axis = (0, 1, 0),
     limits = (0, -0.04)
 )
@@ -87,10 +88,17 @@ joint8 = SliderJoint(
 joint9 = SliderJoint(
     parent = hand,
     child = finger2,
-    transform = Transform(z = -0.0584, yaw = pi),
+    pose = Pose(translation = (0, 0, -0.0584), rotation = (0, 0, pi)),
     axis = (0, 1, 0),
     limits = (0, -0.04)
 )
 
 joints = [joint0, joint1, joint2, joint3, joint4, joint5, joint6, joint7, joint8, joint9]
-franka_research_3 = Model(name = "franka_research_3", joints = joints, base = link0)
+
+# Model below uses a pixi project root directory, but could be adapted to any path setup
+pixi_root = os.getenv("PIXI_PROJECT_ROOT")
+if pixi_root is not None:
+    path = pixi_root + "/marionette_models/franka_research_3/"
+else:
+    raise ValueError("PIXI_PROJECT_ROOT environment variable is not set")
+MODEL = Model(name = "franka_research_3", joints = joints, base = link0, path = path)
